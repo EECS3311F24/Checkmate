7f56f7ba62c77811fa87f3069188ef9474d096d4
TotalDarkness <totaldarkness@totaldarkness.net>
2024-10-12 03:09:39 -0400

    add functional signup page

--- src/frontend/src/components/SignupComponent.jsx
+++ src/frontend/src/components/SignupComponent.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createUser } from '../services/UserService';
+import { useNavigate } from 'react-router-dom';
 
 const SignupComponent = () => {
-  return (
-    <div>SignupComponent</div>
-  )
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+
+    const [errors, setErrors] = useState({
+        username: '',
+        email: ''
+    })
+
+    const navigator = useNavigate();
+
+    function saveUser(e) {
+        e.preventDefault();
+        if (validateForm()) {
+            const user = { username, email }
+            createUser(user).then((response) => {
+                console.log(response.data);
+                navigator("/users")
+            })
+        }
+    }
+
+    function validateForm() {
+        let valid = true;
+        const errorsCopy = { ...errors }
+
+        if (username.trim()) {
+            errorsCopy.username = '';
+        } else {
+            errorsCopy.username = "Username is required!"
+            valid = false;
+        }
+
+        if (email.trim()) {
+            errorsCopy.email = '';
+        } else {
+            errorsCopy.email = "Email is required!"
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+        return valid;
+    }
+
+    return (
+        <div className='container'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+                <h2 className='text-center'>Signup</h2>
+                <div className='card-body'>
+                    <form>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Username</label>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                name='username'
+                                value={username}
+                                className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                                onChange={(e) => setUsername(e.target.value)}
+                            >
+                            </input>
+                            {errors.username && <div className='invalid-feedback'> {errors.username} </div>}
+                        </div>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Email</label>
+                            <input
+                                type="text"
+                                placeholder="Email"
+                                name='email'
+                                value={email}
+                                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </input>
+                            {errors.email && <div className='invalid-feedback'> {errors.email} </div>}
+                        </div>
+                        <button className='btn btn-success' onClick={saveUser}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SignupComponent
--- src/frontend/src/services/UserService.js
+++ src/frontend/src/services/UserService.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const REST_API_BASE_URL = 'http://localhost:8080/api/v1/user';
 
 export const listUsers = () => axios.get(REST_API_BASE_URL);
+
+export const createUser = (user) => axios.post(REST_API_BASE_URL, user);

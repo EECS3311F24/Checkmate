7e885dac34dcfd8aa1c3b194a718b8bd132b8d58
TotalDarkness <totaldarkness@totaldarkness.net>
2024-10-12 02:04:15 -0400

    change to follow more REST Api

--- src/main/java/ca/yorku/checkmate/Controller/UserController.java
+++ src/main/java/ca/yorku/checkmate/Controller/UserController.java
@@ -3,10 +3,8 @@ package ca.yorku.checkmate.Controller;
 import ca.yorku.checkmate.Model.User;
 import ca.yorku.checkmate.Model.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +14,10 @@ import java.util.List;
  * that allows viewing, adding, editing, or deleting users
  */
 @CrossOrigin("*")
-@Controller
+@RestController
+@RequestMapping("api/v1/user")
 public class UserController {
+
     private final UserService userService;
 
     @Autowired
@@ -25,83 +25,42 @@ public class UserController {
         this.userService = userService;
     }
 
-    @ResponseBody
-    @GetMapping("/api/v1/user")
+    @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
-    @ResponseBody
-    @GetMapping(path = "/api/v1/user", params = "username")
-    public User getUser(String username) {
+    @GetMapping("{id}")
+    public User getUserById(@PathVariable("id") String id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping(params = "username")
+    public User getUserByUsername(String username) {
         return userService.getUserByUsername(username);
     }
 
-    @GetMapping(path = "/search")
-    public String showSearchUserForm(User user) {
-        return "search-user";
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        if (!userService.addUser(user)) return new ResponseEntity<>(HttpStatus.CONFLICT);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
-    @PostMapping("/search-user")
-    public String searchUser(@Validated User u, BindingResult result, Model model) {
-        User user = userService.getUserByUsername(u.username);
-        if (result.hasErrors()) {
-            return "search-user";
-        }
-        model.addAttribute("user", user);
-        return "user";
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") String id, @RequestBody User user) {
+        userService.updateUser(id, user);
+        return ResponseEntity.ok(user);
     }
 
-    @GetMapping("/signup")
-    public String showSignUpForm(User user) {
-        return "add-user";
-    }
-
-    @PostMapping("/add-user")
-    public String addUser(@Validated User user, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-user";
-        }
-        if (!userService.addUser(user)) return "Username taken!";
-        model.addAttribute("username", user.getUsername());
-        return "user";
-    }
-
-    @GetMapping("/edit/{ID}")
-    public String showUpdateForm(@PathVariable("ID") String ID, Model model) {
-        User user = userService.getUserByID(ID);
-
-        if (user == null) return "Error! No user by " + ID;
-
-        model.addAttribute("user", user);
-        return "update-user";
-    }
-
-    @PostMapping("/update/{ID}")
-    public String updateUser(@PathVariable("ID") String ID, @Validated User user, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "update-user";
-        }
-
-        userService.updateUser(ID, user);
-        return "redirect:/";
-    }
-
-    @GetMapping("/delete/{ID}")
-    public String deleteUser(@PathVariable("ID") String ID) {
-        User user = userService.getUserByID(ID);
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") String id) {
+        User user = userService.getUserById(id);
         userService.deleteUser(user);
-        return "redirect:/";
+        return ResponseEntity.ok("Deleted user " + user + "!");
     }
 
-    @GetMapping("/delete-all")
-    public String deleteAll() {
-        return "delete-all";
-    }
-
-    @PostMapping("/clear-database")
-    public String clearDatabase() {
-        userService.deleteAll();
-        return "redirect:/";
+    @DeleteMapping
+    public ResponseEntity<String> deleteAll() {
+        return ResponseEntity.ok("Deleted all users");
     }
 }

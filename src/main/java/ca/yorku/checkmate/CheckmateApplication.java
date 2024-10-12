4b3715ec66d62a366a96a67024446856e490329c
TotalDarkness <totaldarkness@totaldarkness.net>
2024-10-12 04:44:28 -0400

    remove old frontend implementation
    Removes old non-functional implementation of frontend

--- src/main/java/ca/yorku/checkmate/CheckmateApplication.java
+++ src/main/java/ca/yorku/checkmate/CheckmateApplication.java
@@ -1,8 +1,5 @@
 package ca.yorku.checkmate;
 
-import ca.yorku.checkmate.Model.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -12,7 +9,6 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 public class CheckmateApplication {
 
 	public static void main(String[] args) {
-		System.out.println("http://localhost:8080");
-		SpringApplication.run(CheckmateApplication.class, args);;
+		SpringApplication.run(CheckmateApplication.class, args);
 	}
 }
--- src/main/java/ca/yorku/checkmate/Controller/IndexController.java
+++ src/main/java/ca/yorku/checkmate/Controller/IndexController.java
@@ -2,7 +2,6 @@ package ca.yorku.checkmate.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Simple controller that displays the index page

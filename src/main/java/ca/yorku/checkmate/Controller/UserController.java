147f09801a508079c34175bef69de356655ea3d5
TotalDarkness <totaldarkness@totaldarkness.net>
2024-10-25 13:35:48 -0400

    update rest api, add documentation
    Update api to follow best practices, add documentation explaining what api does

--- src/main/java/ca/yorku/checkmate/Controller/UserController.java
+++ src/main/java/ca/yorku/checkmate/Controller/UserController.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
+ * <p>Endpoint is api/v1/user/</p>
  * Controller for user information, providing a rest endpoint
  * that allows viewing, adding, editing, or deleting users
  */
 @CrossOrigin("*")
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("api/v1/user/")
 public class UserController {
-
     private final UserService userService;
 
     @Autowired
@@ -25,33 +25,76 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * <p>URL: api/v1/user/</p>
+     * Gets all users in database.
+     * @return a List of users.
+     */
     @GetMapping
     public List<User> getUsers() {
+        // TODO user Response Entity?
         return userService.getUsers();
     }
 
+    /**
+     * <p>URL: api/v1/user/{id}<p>
+     * Gets user by id in database.
+     * @param id The id of the user.
+     * @return The user associated with the id if it exists.
+     */
     @GetMapping("{id}")
     public User getUserById(@PathVariable("id") String id) {
+        // TODO user Response Entity?
         return userService.getUserById(id);
     }
 
+    /**
+     * <p>URL: api/v1/user?username={username}</p>
+     * Gets user by username in database.
+     * @param username The username of the user.
+     * @return The user associated with the username if it exists.
+     */
     @GetMapping(params = "username")
     public User getUserByUsername(String username) {
+        // TODO user Response Entity?
         return userService.getUserByUsername(username);
     }
 
+    /**
+     * <p>URL: api/v1/user/</p>
+     * Create a new user in the database.
+     * @param user The user to be created.
+     * @return A response entity with user, informing client
+     * with Http status 201 if created or 409 if not created.
+     */
     @PostMapping
-    public ResponseEntity<User> addUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         if (!userService.addUser(user)) return new ResponseEntity<>(HttpStatus.CONFLICT);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    /**
+     * <p>URL: api/v1/user/{id}</p>
+     * Update a user by id in the database.
+     * @param id The id of the user.
+     * @param user The user to be created.
+     * @return A response entity with user, informing client
+     * with Http status 200 if updated, 201 if created, 409 if not created.
+     */
     @PutMapping("{id}")
     public ResponseEntity<User> updateUser(@PathVariable("id") String id, @RequestBody User user) {
+        if (!userService.hasUserById(id)) return createUser(user);
         userService.updateUser(id, user);
         return ResponseEntity.ok(user);
     }
 
+    /**
+     * <p>URL: api/v1/user/{id}</p>
+     * Delete a user by id in the database.
+     * @param id The id of the user.
+     * @return A response entity with a message, informing client
+     * with Http status 200.
+     */
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") String id) {
         User user = userService.getUserById(id);
@@ -59,6 +102,12 @@ public class UserController {
         return ResponseEntity.ok("Deleted user " + user + "!");
     }
 
+    /**
+     * <p>URL: api/v1/user/</p>
+     * Delete all users in the database.
+     * @return A response entity with a message, informing client
+     * with Http status 200.
+     */
     @DeleteMapping
     public ResponseEntity<String> deleteAll() {
         return ResponseEntity.ok("Deleted all users");

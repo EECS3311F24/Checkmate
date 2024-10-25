4745bd3736633cdba838ce16e37b8e9a76eaba4b
TotalDarkness <totaldarkness@totaldarkness.net>
2024-10-25 14:38:36 -0400

    add basic ChessBoard model

---
+++ src/main/java/ca/yorku/checkmate/Model/ChessBoard.java
@@ -0,0 +1,21 @@
+package ca.yorku.checkmate.Model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+/**
+ * ChessBoard model that represents a simple chess board.
+ */
+public class ChessBoard {
+    @Id
+    public String id;
+    @CreatedDate
+    public Date createdOn;
+
+    @Override
+    public String toString() {
+        return String.format("ChessBoard[id=%s, createdOn='%s']", id, createdOn);
+    }
+}

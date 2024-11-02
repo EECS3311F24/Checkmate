c1821df7d4479d58821fe7013a700f28dac6f651
TotalDarkness <totaldarkness@totaldarkness.net>
2024-11-02 04:06:46 -0400

    move console input to controller tester

--- src/main/java/ca/yorku/checkmate/Model/chess/Controller.java
+++ src/main/java/ca/yorku/checkmate/Model/chess/Controller.java
@@ -2,8 +2,15 @@ package ca.yorku.checkmate.Model.chess;
 
 import ca.yorku.checkmate.Model.chess.chesspieces.ChessPiece;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // TODO remove only for testing/printing to console
 public class Controller {
+    private static final String INVALID_INPUT_MESSAGE = "Invalid number, please enter 1-8";
+    private static final String IO_ERROR_MESSAGE = "I/O Error";
+    private static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
     private Chess chess;
     private Player pW;
     private Player pB;
@@ -23,14 +30,8 @@ public class Controller {
             Move to = null;
             Player whosTurn = chess.getWhosTurn();
 
-            if (whosTurn.getPlayerColor() == ChessBoard.white) {
-                from = pW.getMove();
-                to = pW.getMove();
-            }
-            if (whosTurn.getPlayerColor() == ChessBoard.black) {
-                from = pB.getMove();
-                to = pB.getMove();
-            }
+            from = getMove();
+            to = getMove();
             this.reportMove(whosTurn.getPlayerColor(), from, to);
             ChessPiece piece = this.chess.getChessPiece(from.getRow(), from.getColumn());
             if(piece != null) {
@@ -59,6 +60,37 @@ public class Controller {
         String s = chess.getChessBoard().toString() +
                 "  " + chess.getWinner() + " won\n";
         System.out.println(s);
+    }
+
+    public Move getMove() {
+        int row = getMove("row: ");
+        int col = getMove("col: ");
+        return new Move(row, col);
+    }
+
+    private int getMove(String message) {
+        int move, lower = 0, upper = 7;
+        while (true) {
+            try {
+                System.out.print(message);
+                String line = stdin.readLine();
+                line = line.trim();
+                line = line.strip();
+                System.out.println(line);
+                move = Integer.parseInt(line);
+                if (lower <= move && move <= upper) {
+                    return move;
+                } else {
+                    System.out.println(INVALID_INPUT_MESSAGE);
+                }
+            } catch (IOException e) {
+                System.out.println(INVALID_INPUT_MESSAGE);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println(INVALID_INPUT_MESSAGE);
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
--- src/main/java/ca/yorku/checkmate/Model/chess/Player.java
+++ src/main/java/ca/yorku/checkmate/Model/chess/Player.java
@@ -8,46 +8,8 @@ import java.io.InputStreamReader;
 public class Player {
     private final char playerColor;
 
-    // TODO remove only for testing/printing to console
-    private static final String INVALID_INPUT_MESSAGE = "Invalid number, please enter 1-8";
-    private static final String IO_ERROR_MESSAGE = "I/O Error";
-    private static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-
     public Player(char playerColor) {
         this.playerColor = playerColor;
-    }
-
-    public Move getMove() {
-        int row = getMove("row: ");
-        int col = getMove("col: ");
-        return new Move(row, col);
-    }
-
-    // TODO remove only for testing/printing to console
-    private int getMove(String message) {
-
-        int move, lower = 0, upper = 7;
-        while (true) {
-            try {
-                System.out.print(message);
-                String line = Player.stdin.readLine();
-                line = line.trim();
-                line = line.strip();
-                System.out.println(line);
-                move = Integer.parseInt(line);
-                if (lower <= move && move <= upper) {
-                    return move;
-                } else {
-                    System.out.println(INVALID_INPUT_MESSAGE);
-                }
-            } catch (IOException e) {
-                System.out.println(INVALID_INPUT_MESSAGE);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println(INVALID_INPUT_MESSAGE);
-            }
-        }
-        return -1;
     }
 
     public char getPlayerColor() {

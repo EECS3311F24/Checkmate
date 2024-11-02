61d07510790efbae12df9d3e0669c131a4ed4032
TotalDarkness <totaldarkness@totaldarkness.net>
2024-11-02 04:28:18 -0400

    convert Move to record

--- src/main/java/ca/yorku/checkmate/Model/chess/Move.java
+++ src/main/java/ca/yorku/checkmate/Model/chess/Move.java
@@ -1,15 +1,7 @@
 package ca.yorku.checkmate.Model.chess;
 
 // TODO record?
-public class Move {
-    private final int x;
-    private final int y;
-
-    public Move(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-
+public record Move(int x, int y) {
     public int getRow() {
         return x;
     }

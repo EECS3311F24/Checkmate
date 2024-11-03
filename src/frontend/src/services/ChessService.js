6b2c34299b5400e3686a6e7e4ded86dbcae2c7e9
TotalDarkness <totaldarkness@totaldarkness.net>
2024-11-03 00:01:16 -0400

    add more rest api calls for chessboard

--- src/frontend/src/services/ChessService.js
+++ src/frontend/src/services/ChessService.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
-const CHESS_API_BASE_URL = "http://localhost:8080/api/game";
+const CHESS_API_BASE_URL = "http://localhost:8080/api/v1/boards";
 
-export const startGuestGame = () => {
-    return axios.post(`${CHESS_API_BASE_URL}/guest/start`);
-}
+export const startGuestGame = () => axios.put(CHESS_API_BASE_URL);
 
-export const makeMove = (from, to) => {
-    return axios.post(`${CHESS_API_BASE_URL}/move`, { from, to });
-}
+export const makeMove = (id, from, to) => axios.post(CHESS_API_BASE_URL + '/' + id, { from, to });
 
 export const getPossibleMoves = (row, col) => {
     return axios.get(`${CHESS_API_BASE_URL}/moves?row=${row}&col=${col}`);

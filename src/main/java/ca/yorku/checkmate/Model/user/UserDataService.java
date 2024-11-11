a7192dc18968f441a3a79b9a084634aac5b6118a
TotalDarkness <totaldarkness@totaldarkness.net>
2024-11-10 22:56:27 -0500

    07 add getUserData to get user data using id

--- src/main/java/ca/yorku/checkmate/Model/user/UserDataService.java
+++ src/main/java/ca/yorku/checkmate/Model/user/UserDataService.java
@@ -3,6 +3,8 @@ package ca.yorku.checkmate.Model.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserDataService {
     private final UserDataRepository repository;
@@ -10,6 +12,10 @@ public class UserDataService {
     @Autowired
     public UserDataService(UserDataRepository repository) {
         this.repository = repository;
+    }
+
+    public Optional<UserData> getUserData(String id) {
+        return repository.findById(id);
     }
 
     public boolean saveUserData(UserData userData) {

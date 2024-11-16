fc31ecaad9213932425f563e0a756023600384e3
TotalDarkness <totaldarkness@totaldarkness.net>
2024-11-16 14:35:48 -0500

    07 add account button and hide buttons when logged in

--- src/frontend/src/components/HeaderComponent.jsx
+++ src/frontend/src/components/HeaderComponent.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { Col, Row, Button, Form, Nav, Navbar } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react'
+import { Form, Nav, Navbar } from 'react-bootstrap';
 import { getTranslation, useLanguage } from './LanguageProvider';
+import { getAuthenticate } from '../services/UserService';
 
 const HeaderComponent = () => {
   const { language, setLanguage } = useLanguage();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    getUser();
+  }, [])
+
+  function getUser() {
+    getAuthenticate().then(response => {
+      setUser(response.data)
+    }).catch(e => {})
+  }
 
   return (
     <Navbar className="navbar navbar-dark bg-dark justify-content-between">
@@ -11,9 +23,10 @@ const HeaderComponent = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
+          <Nav.Link href={"/account/" + user?.id} hidden={!user} >{getTranslation("HeaderComponentAccount", language) + ": " + user?.username}</Nav.Link>
           <Nav.Link href="/users">{getTranslation("HeaderComponentUsers", language)}</Nav.Link>
-          <Nav.Link href="/signup">{getTranslation("HeaderComponentSignup", language)}</Nav.Link>
-          <Nav.Link href="/login">{getTranslation("HeaderComponentLogin", language)}</Nav.Link>
+          <Nav.Link href="/signup" hidden={user}>{getTranslation("HeaderComponentSignup", language)}</Nav.Link>
+          <Nav.Link href="/login" hidden={user}>{getTranslation("HeaderComponentLogin", language)}</Nav.Link>
           <Nav.Link href="/play">{getTranslation("HeaderComponentPlayChess", language)}</Nav.Link>
         </Nav>
         <Form.Select
